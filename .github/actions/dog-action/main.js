e1404ccfa73dbc587d3ffb89b311a970906f85e0
digracesion <mggicay@gmail.com>
2023-05-06 20:32:43 +0900

    changed path for dog-action main.js

--- .github/actions/dog-action/main.js
+++ .github/actions/dog-action/main.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
-const getDogList = require("../../../write-javascript-actions/.github/actions/dog-action/dog");
+const getDogList = require("./dog");
+
 async function run() {
     const dog = await getDogList();
     console.log(dog);
